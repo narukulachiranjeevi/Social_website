6c5508d074cd1b0f3bf0e757b23f4e6ddef55d41
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-09 16:48:54 +0530

    changed variable names

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
 
+
+
 urlpatterns = [
     ##path('login/',views.user_login,name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
@@ -14,4 +16,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
+
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, UserEditForm, ProfileEditForm
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
+from .models import Profile
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 @login_required
@@ -52,7 +54,26 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html',{'new_user': new_user})
     else:
         user_form = RegisterForm()
     return render(request,'account/register.html',{'user_form': user_form})
+
+@login_required
+def edit(request):
+    print('entered')
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request,'Successfully Updated')
+        else :
+            messages.error(request, 'Error updating your profile')            
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+        instance=request.user.profile)
+    return render(request,'account/edit.html', {'user_form': user_form,'profile_form': profile_form})
