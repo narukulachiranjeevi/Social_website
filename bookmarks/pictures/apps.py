ba8f3f4aae31b60f16d1439465761afbb6e12b31
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-12 17:40:27 +0530

    using signals for denormalizing counts

--- bookmarks/pictures/apps.py
+++ bookmarks/pictures/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PicturesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pictures'
+    def ready(self):
+        # import signal handlers
+        import pictures.signals
