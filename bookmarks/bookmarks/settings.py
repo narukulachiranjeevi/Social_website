5e67fbe7fa2a0ad72db786f3abb48259d61d42a2
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-13 11:24:25 +0530

    Storing a ranking and total_views in Redis

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -158,3 +158,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Googl
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user':lambda u: reverse_lazy('user_detail',args=[u.username])
 }
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
